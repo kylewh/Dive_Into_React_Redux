32ae307a49371adc0b19365abd8d133db5916d98
kylewh <kylevanho@gmail.com>
2017-05-03 18:04:41 +0800

    fix intro and delete video background for faster loading

--- Weather/README.md
+++ Weather/README.md
@@ -1,14 +1,22 @@
 ## 一个天气搜索应用
 
-Preview:    
-![](http://om8hmotom.bkt.clouddn.com/weather.gif)
+###Preview:    
+![](http://om8hmotom.bkt.clouddn.com/2017-05-03-weather.gif)
 
-Tech Stack:
+###Tech Stack:
 
 - react
 - redux
 - axios
-- sparkLine
+- react-sparklines
+- google-map
 
+###简介
+学习过程中的小demo，主要用来练习异步action的发起方法，所以暂时不考虑请求过程中的其他情况，为后期做todo应用打基础。 这里使用是googlemap，weatherAPI使用的也是国外的，所以本地运行时需要翻墙.
 
-这是一个教程中的个人练习demo
+###思考
+延伸考虑可以优化的点：
+
+- 显示loading状态
+- error handle and present
+- various view mode (Add filter)
--- Weather/src/components/app.js
+++ Weather/src/components/app.js
@@ -7,23 +7,22 @@ import '../../public/style/style.css'
 export default class App extends Component {
   render() {
     return (
-      <div className='hero'>
+      <div className='hero main-ct'>
         <div className="hero-head">
           <header className="nav">
             <div className="container is-info">
               <div className="nav-left">
                 <a className="nav-item">
-                  <img src="../public/img/log.png" alt="Logo" />
+                  <img src="http://om8hmotom.bkt.clouddn.com/log.png" alt="Logo" />
                 </a>
               </div>
             </div>
           </header>
         </div>
+        <div className='weather-bg'></div>
         <div className="hero-body">
-          <video autoPlay="autoplay" loop="loop">  
-            <source  src="../public/video/The_Slope.webm" type="video/webm" />;  
-          </video>  
-          <div className="container has-text-centered">
+
+          <div className="container weather-data has-text-centered">
             <SearchBar />
             <WeatherList />
           </div>
